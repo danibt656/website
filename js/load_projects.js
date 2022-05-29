87fd44597afaf1f5a1268d2aff8aaf8d4d26ea96
danibt656 <dbmartin656@gmail.com>
2022-05-29 19:38:21 +0200

    New look

--- js/load_projects.js
+++ js/load_projects.js
@@ -3,18 +3,18 @@
  * Load projects from JSON
  */
 $(document).ready(function(){
-    $.getJSON("docs/project_data.json", function( data ) {
+    $.getJSON("../docs/project_data.json", function( data ) {
         for (var i in data) {
             console.log(data[i]);
 
             const project_desc = data[i]["project_desc"];
             const project_name = data[i]["project_name"];
-            const img = data[i]["img"];
+            const img = '../' + data[i]["img"];
             const github_url = data[i]["github_url"];
             const text = data[i]["text"];
             const date = data[i]["date"];
             
-            let lang = "imgs/" + data[i]["lang"] + ".png";
+            let lang = "../imgs/" + data[i]["lang"] + ".png";
 
             let target_window = "";
             if (data[i]["openInNewTab"])
@@ -23,23 +23,30 @@ $(document).ready(function(){
             project_template = `
             <div class="floating-box-project">
                 <div class="horizontal-flex">
+                    <div class="image-cropper-border">
+                        <img src="`+img+`" />
+                    </div>
+
+                    <p style="margin-left: 20px;"></p>
+
                     <div class="align-left">
                         <div class="vertical-flex">
                             <h3 class="project-desc">`+project_desc+`</h3>
                             <div style="padding-top: 20px"></div>
                             <h4 class="project-description">`+text+`</h4>
                         </div>
-                        <a href="`+github_url+`" `+target_window+` class="fa fa-github"></a>
                     </div>
 
                     <div style="padding-left: 40px"></div>
 
                     <div class="vertical-flex">
-                        <div class="image-cropper">
-                            <img src="`+img+`" class="rounded" />
-                        </div>
                         <h1 class="project-title">`+project_name+`</h1>
-                        <img src="`+lang+`" class="language-icon" />
+                        <table>
+                        <tr>
+                        <td><img src="`+lang+`" class="language-icon" /></td>
+                        <td><a href="`+github_url+`" `+target_window+` class="fa fa-github"></a></td>
+                        <tr>
+                        </table>
                         <p><i>`+date+`</i></p>
                     </div>
                 </div>
